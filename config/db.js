ac982830e8e6c89c902d2cea2ddffb3f46bfc8ad
kaleem7864 <kaleem.nalband@gmail.com>
2023-01-12 21:00:05 +0530

    mongodb url updated

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 // const config=require('config');
 // require('dotenv').config()
 
 // const db=config.get('mongoURI');
 
 //**PROTECT CREDS WITH THIS .ENV INSTEAD OF BRADS' DEFAULTJSON
- const db = process.env.MY_MONGO_URI;
+const db =
+  "mongodb+srv://kaleem:kaleem@cluster0.8kszcwb.mongodb.net/?retryWrites=true&w=majority";
 
-const connectDB =async()=>{
-    try {
-
-        await mongoose.connect(db);
-        console.log('MongoDB is Connected...')
-    } catch (err) {
-        console.error(err.message);
-        console.log('Check Your ENV VAR')
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    console.log("Check Your ENV VAR");
+    process.exit(1);
+  }
+};
 module.exports = connectDB;
